4c79d173aa71a3a50e4949e0448add18d208fc31
asifshuvo119181@gmail.com <asifshuvo119181@gmail.com>
2021-09-20 04:18:36 +0600

    count done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+
 import './App.css';
+import Control from './components/Control/Control';
+
+import Count from './components/count/Count';
+
+import store from './store';
 
 function App() {
+
+  //A reducer function must have 2 parameters 
+  //state , action 
+
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Provider store={store}>
+      <div className="App">
+      <h1>React-Redux Crash Course</h1>
+      <Count/>
+      <Control></Control>
+      
     </div>
+   
+    </Provider>
   );
 }
 
-export default App;
+export default App
+
+//store -> will hold of our application data/state
+// Reducer -> A function who return a specofoc amount of state or data 
+//Actions -> Event occurs -> {type:"something" payload:''}
+//Dispatcher
+//Subscriber
+
+//1. Create a reducer
+//2. Create store with the help of reducer
+//3. Now we can subscribe 
+//4. dispatch(action)
+
+//React will be responsible for only view layer 
+//Redux will be responsible for only data layer 
+
+//React - Redux
